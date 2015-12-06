bdef2adf64b64a892d478acbdc6f05790d0f339d
Simon Garnier <simongarnierg@gmail.com>
2015-12-06 12:07:26 -0500

    bootrap and jquery deps

--- index.jsx
+++ index.jsx
@@ -1,7 +1,10 @@
 /** @jsx React.DOM */
 'use strict'
 
+global.jQuery = require('jquery');
+
 var React = require('react')
+var bootstrap = require('bootstrap')
 var ReactDOM = require('react-dom')
 var Hello = require('./components/Hello')
 
