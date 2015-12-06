6adfb9dd6d593859dc2bd833b790dbafcab7cce2
Simon Garnier <simongarnierg@gmail.com>
2015-12-06 02:20:32 -0500

    entry point

---
+++ index.jsx
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Hello = require('./components/Hello')
+
+ReactDOM.render(<Hello />, document.getElementById('content'))
